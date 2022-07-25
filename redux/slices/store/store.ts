a8319dfdbd70983b69826fa181333424a8d9418f
Orkin Evgenij <orkinevgenij@gmail.com>
2022-07-25 23:34:01 +0300

    test

--- redux/slices/store/store.ts
+++ redux/slices/store/store.ts
@@ -1,20 +1,8 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import movieSlice from "../movieSlice";
 import serialsSlice from "../serialsSlice";
 import topFilmsSlice from "../topFilmsSlice";
 
-// const rootReducer = combineReducers({
-//   movies: movieSlice,
-// });
-// const store = configureStore({
-//   reducer: rootReducer,
-// });
-// export type RootState = ReturnType<typeof rootReducer>;
-
-// export type AppDispatch = typeof store.dispatch;
-// export const useReduxDispatch = (): AppDispatch => useDispatch<AppDispatch>();
-// export const useReduxSelector: TypedUseSelectorHook<RootState> = useSelector;
-// export default store;
 const store = configureStore({
   reducer: {
     movies: movieSlice,
